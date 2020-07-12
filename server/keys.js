476442d9f89e0cef25e48d5ebe3a9806610a795a
ApoorvaJu08 <apoorva10339@gmail.com>
2020-07-12 16:30:09 +0530

    api part done

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://apoorva:mXh0Z8y0rDQ4c6w4@cluster0-7gsir.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET: "s$dfg#h&j!56(#$$$#)^R%"
+}
